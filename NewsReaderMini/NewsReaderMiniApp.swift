5c32bfe396af456d153d456e95c475637a6ca1e2
Chad Newbry <chad.newbry@gmail.com>
2022-12-12 14:25:58 -0500

    Initial Commit

---
+++ NewsReaderMini/NewsReaderMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderMiniApp.swift
+//  NewsReaderMini
+//
+//  Created by Chad Newbry on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
