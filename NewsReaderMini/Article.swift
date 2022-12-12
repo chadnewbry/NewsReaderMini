6ba32e117c4d751a206d3b156ab2df7bddb9a13f
Chad Newbry <chad.newbry@gmail.com>
2022-12-12 14:37:09 -0500

    Initial Commit.

---
+++ NewsReaderMini/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  NewsReaderMini
+//
+//  Created by Chad Newbry on 12/12/22.
+//
+
+import Foundation
+
+struct Article {
+    var thumbnailImage: String
+    var publisher: String
+    var title: String
+}
---
+++ NewsReaderMini/ArticleDetail.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleDetail.swift
+//  NewsReaderMini
+//
+//  Created by Chad Newbry on 12/12/22.
+//
+
+import SwiftUI
+
+struct ArticleDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleDetail()
+    }
+}
